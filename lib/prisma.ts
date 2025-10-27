536ea09a2dd910279a46705473956fdbaa62f12d
charaivati <charaivati.forward@gmail.com>
2025-10-28 03:54:48 +0530

    desperate

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,19 +2,20 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  // allow global var across hot reloads in dev
-  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-// Create a single Prisma client instance globally
 export const prisma =
   global.prisma ??
   new PrismaClient({
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "info", "warn", "error"]
-        : ["error"], // only log errors in production
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
   });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+export default prisma;
